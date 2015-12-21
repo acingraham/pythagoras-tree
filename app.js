a2efb4eb80e271db32c1e1c8ead5a8fbdf39d794
Andrew Ingraham <andrewingraham@Andrews-MacBook-Air-2.local>
2015-12-21 01:41:25 -0500

    Calculate width and height in CSS

--- app.js
+++ app.js
@@ -2,25 +2,22 @@ console.time('tree');
 
 // TODO - Return the entire HTML for that depth.  
 var map = {};
-function getData(depth, width) {
+function getData(depth) {
 
     var color,
         left,
-        nextWidth,
         right,
         style;
 
     if (!map[depth]) {
 
         color = 'rgba(' + [0, Math.floor(depth * 25), 100, (1 - depth / 40)].join(',') + ');';
-        nextWidth = Math.sqrt(2 * Math.pow(width / 2, 2));
-        style = 'width: ' + nextWidth + 'px;height: ' + nextWidth + 'px;background-color: ' + color;
+        style = 'background-color: ' + color;
         left = $('<div class="block left" style="' + style + '"></div>');
         right = $('<div class="block right" style="' + style + '"></div>');
 
         map[depth] = {
             left: left,
-            nextWidth: nextWidth,
             right: right
         };
     }
@@ -28,8 +25,8 @@ function getData(depth, width) {
     return map[depth];
 }
 
-function drawBlock(parent, width, depth) {
-    var data  = getData(depth, width),
+function drawBlock(parent, depth) {
+    var data  = getData(depth),
         left  = data.left.clone(),
         right = data.right.clone();
 
@@ -37,12 +34,12 @@ function drawBlock(parent, width, depth) {
     parent.append(right);
 
     if (depth > 0) {
-        drawBlock(left, data.nextWidth, depth - 1);
-        drawBlock(right, data.nextWidth, depth - 1);
+        drawBlock(left, depth - 1);
+        drawBlock(right, depth - 1);
     }
 
 }
 
 // TODO - Append to DOM once it's completely built out
-drawBlock($('#root'), 100, 10);
+drawBlock($('#root'), 10);
 console.timeEnd('tree');
